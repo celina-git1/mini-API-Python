7d7ca7741059a343505780b9a30b445a662c7a68
Celina Habtiche <celina.habtiche@esme.fr>
2025-05-11 10:38:06 +0200

    Mise à jour de app.py

--- app.py
+++ app.py
@@ -3,56 +3,44 @@ from datetime import datetime
 from google.cloud import aiplatform
 import os
 import vertexai
-
+ 
 app = Flask(__name__)
-
+ 
+# Route hello
 @app.route("/hello", methods=["GET"])
 def hello():
     return jsonify({"message": "Découvrez notre mini API Flask"})
-
+ 
+# Route status
 @app.route("/status", methods=["GET"])
 def status():
     now = datetime.now().isoformat()
     return jsonify({"status": "API fonctionnelle", "timestamp": now})
-
+ 
+# Route data GET
 @app.route("/data", methods=["GET"])
 def get_data():
     return jsonify({"message": "Accès aux données depuis GCS à intégrer"})
-
+ 
+# Route data POST
 @app.route("/data", methods=["POST"])
 def post_data():
     return jsonify({"message": "Ajout des données dans GCS à implémenter"})
-
-
-import os
-from vertexai.preview.generative_models import GenerativeModel, ChatSession
-from flask import Flask
  
-app = Flask(__name__)
- 
-
+# Initialisation de VertexAI
 vertexai.init(project=os.environ.get("PROJECT_ID"), location=os.environ.get("REGION"))
  
-
+# Initialisation du modèle
 model = GenerativeModel(model_name="gemini-2.0-flash-001")
- 
-
 chat: ChatSession = model.start_chat()
  
+# Route joke
 @app.route("/joke")
 def joke():
-    
     prompt = "Raconte une blague drôle et courte."
- 
-    
     response = chat.send_message(prompt)
- 
-    
     return {"joke": response.text}
  
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+# Lancement du serveur Flask
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-
